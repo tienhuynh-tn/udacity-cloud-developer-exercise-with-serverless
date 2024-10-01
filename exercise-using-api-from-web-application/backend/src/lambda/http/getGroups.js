9a76e2ad2dc928bf3024fb49d769ef1c79599118
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-10-02 01:28:22 +0700

    Connecting API To DynamoDB

--- exercise-using-api-from-web-application/backend/src/lambda/http/getGroups.js
+++ exercise-using-api-from-web-application/backend/src/lambda/http/getGroups.js
@@ -1,23 +1,35 @@
-const data = [
-  {
-    id: '1',
-    name: 'Dogs',
-    description: 'Only dog images here!'
-  },
-  {
-    id: '2',
-    name: 'Nature',
-    description: 'What can be a better object for photography'
-  },
-  {
-    id: '3',
-    name: 'Cities',
-    description: 'Creative display of urban settings'
-  }
-]
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+
+//const data = [
+//  {
+//    id: '1',
+//    name: 'Dogs',
+//    description: 'Only dog images here!'
+//  },
+//  {
+//    id: '2',
+//    name: 'Nature',
+//    description: 'What can be a better object for photography'
+//  },
+//  {
+//    id: '3',
+//    name: 'Cities',
+//    description: 'Creative display of urban settings'
+//  }
+//]
+
+const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
+
+const groupsTable = process.env.GROUPS_TABLE
 
 export async function handler(event) {
   console.log('Processing event: ', event)
+
+  const result = await dynamoDbClient.scan({
+    TableName: groupsTable
+  })
+  const items = result.Items
 
   return {
     statusCode: 200,
@@ -25,7 +37,8 @@ export async function handler(event) {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items: data
+//      items: data
+       items
     })
   }
 }
