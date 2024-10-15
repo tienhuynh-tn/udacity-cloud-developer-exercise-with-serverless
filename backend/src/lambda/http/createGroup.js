d016d159c71caca5d12cb83894ce5afd1bb3af8f
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-10-15 18:01:25 +0700

    Hexagonal Architecture

--- backend/src/lambda/http/createGroup.js
+++ backend/src/lambda/http/createGroup.js
@@ -1,38 +1,30 @@
-import { DynamoDB } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
-import { v4 as uuidv4 } from 'uuid'
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
 import { getUserId } from '../auth/utils.mjs'
+import { createGroup } from '../../businessLogic/groups.mjs'
 
-const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
-
-const groupsTable = process.env.GROUPS_TABLE
-
-export async function handler(event) {
-  console.log('Processing event: ', event)
-  const itemId = uuidv4()
-
-  const parsedBody = JSON.parse(event.body)
-
-  const authorization = event.headers.Authorization
-  const userId = getUserId(authorization)
-  const newItem = {
-    id: itemId,
-    userId,
-    ...parsedBody
-  }
-
-  await dynamoDbClient.put({
-    TableName: groupsTable,
-    Item: newItem
-  })
-
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      newItem
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
     })
-  }
-}
+  )
+  .handler(async (event) => {
+    console.log('Processing event: ', event)
+
+    const newGroup = JSON.parse(event.body)
+
+    const authorization = event.headers.Authorization
+    const userId = getUserId(authorization)
+
+    const newItem = await createGroup(newGroup, userId)
+
+    return {
+      statusCode: 201,
+     body: JSON.stringify({
+        newItem
+      })
+    }
+  })
--- backend/src/lambda/http/getGroups.js
+++ backend/src/lambda/http/getGroups.js
@@ -1,12 +1,7 @@
-import { DynamoDB } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 import middy from '@middy/core'
 import cors from '@middy/http-cors'
 import httpErrorHandler from '@middy/http-error-handler'
-
-const dynamoDbClient = DynamoDBDocument.from(new DynamoDB())
-
-const groupsTable = process.env.GROUPS_TABLE
+import { getAllGroups } from '../../businessLogic/groups.mjs'
 
 export const handler = middy()
   .use(httpErrorHandler())
@@ -18,16 +13,12 @@ export const handler = middy()
   .handler(async (event) => {
     console.log('Processing event: ', event)
 
-    const scanCommand = {
-      TableName: groupsTable
-    }
-    const result = await dynamoDbClient.scan(scanCommand)
-    const items = result.Items
+    const groups = await getAllGroups()
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        items
+        items: groups
       })
     }
   })
