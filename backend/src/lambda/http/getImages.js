73aea42dc499dac9ec16c4f56d6b0c45b4e36381
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-10-15 17:30:42 +0700

    Use Middy Middleware

--- backend/src/lambda/http/getImages.js
+++ backend/src/lambda/http/getImages.js
@@ -1,41 +1,46 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
 
 const dynamoDbDocument = DynamoDBDocument.from(new DynamoDB())
 
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
 
-export async function handler(event) {
-  console.log('Caller event', event)
-  const groupId = event.pathParameters.groupId
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(async (event) => {
+    console.log('Caller event', event)
+    const groupId = event.pathParameters.groupId
 
-  const validGroupId = await groupExists(groupId)
+    const validGroupId = await groupExists(groupId)
 
-  if (!validGroupId) {
+    if (!validGroupId) {
+      throw createError(
+        404,
+        JSON.stringify({
+          error: 'Group does not exist'
+        })
+      )
+    }
+
+    const images = await getImagesPerGroup(groupId)
+
     return {
-      statusCode: 404,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
+      statusCode: 201,
       body: JSON.stringify({
-        error: 'Group does not exist'
+        items: images
       })
     }
-  }
-
-  const images = await getImagesPerGroup(groupId)
-
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      items: images
-    })
-  }
-}
+  })
 
 async function groupExists(groupId) {
   const result = await dynamoDbDocument.get({
